63de2505e588f500229a8196f2a55af72766363b
Fran <franncook17@hotmail.co.uk>
2024-08-13 15:17:59 +0000

    Commit 1

--- myApp.js
+++ myApp.js
@@ -8,34 +8,74 @@ mongoose.connect(
   process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true }
 )
 
-// Define the Person model (as a placeholder, since it hasn't been defined yet)
-let Person;
+let personSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  age: Number,
+  favoriteFoods: [String]
+});
 
-// Define the functions (placeholders as given in your code)
-const createAndSavePerson = (done) => {
-  done(null /*, data*/);
+const Person = mongoose.model("Person", personSchema);
+
+var createAndSavePerson = function(done) {
+  var janeFonda =new Person({name: "jane Fonda", age: 84, favoriteFoods: ["eggs", "fish", "fresh fruit"]});
+
+  janeFonda.save(function(err,data){
+    if (err) return console.error(err);
+    done(null,data)
+  });
 };
 
-const createManyPeople = (arrayOfPeople, done) => {
-  done(null /*, data*/);
+var arrayOfPeople = [
+  {name: "Jack", age: 28, favoriteFoods: ["Spaghetti"]},
+  {name: "Melek", age: 28, favoriteFoods: ["Peach"]},
+  {name: "Kirsty", age: 30, favoriteFoods: ["Popcorn"]}
+];
+
+var createManyPeople = function(arrayOfPeople, done) {
+  Person.create(arrayOfPeople, function (err, people) {
+    if (err) return console.log(err);
+    done(null, people);
+  });
 };
 
-const findPeopleByName = (personName, done) => {
-  done(null /*, data*/);
+var findPeopleByName = function(personName, done) {
+Person.find({name: personName}, function (err, personFound) {
+  if (err) return console.log(err);
+  done(null, personFound);
+});
 };
 
-const findOneByFood = (food, done) => {
-  done(null /*, data*/);
+var findOneByFood = function(food, done) {
+Person.findOne({favoriteFoods: food}, function (err, data) {
+if (err) return console.log(err);
+done(null, data);
+});
 };
 
-const findPersonById = (personId, done) => {
-  done(null /*, data*/);
+var findPersonById = function(personId, done) {
+  Person.findById(personId, function (err, data) {
+    if (err) return console.log(err);
+    done(null, data);
+  });
 };
 
 const findEditThenSave = (personId, done) => {
   const foodToAdd = "hamburger";
-  done(null /*, data*/);
+
+Person.findById(personId, (err, person) => {
+  if(err) return console.log(err);
+
+  person.favoriteFoods.push(foodToAdd);
+
+  person.save((err, updatedPerson) => {
+    if(err) return console.log(err);
+    done(null, updatedPerson)
+  })
+})
 };
+
+
+
 
 const findAndUpdate = (personName, done) => {
   const ageToSet = 20;
